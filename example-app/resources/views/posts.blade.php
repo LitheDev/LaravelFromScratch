6e1105625097e6fca968254487e21c0861d2dd7a
LitheDev <101687416+LitheCodes@users.noreply.github.com>
2023-07-16 18:44:48 +1000

    Mass Migration

--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -4,7 +4,7 @@
         <article class="{{$loop->even ? 'test' : ''}}">
             <h1>
                 <a href="/posts/{{ $post -> id}}">
-                    {{$post ->title}}
+                    {!! $post ->title !!}
                 </a>
             </h1>
             <div>
